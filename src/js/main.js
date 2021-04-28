ec069e3b683e06b5d09a0bce8a3f42b936334b09
Lina-Karyzhska <lina.dronova.s@gmail.com>
2021-04-28 22:06:39 +0300

    added exhibitions, changed header and form

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 document.querySelector('.copy').addEventListener('click', function () { 
-            let copyText = document.querySelector('.about-us__copytext');
+            let copyText = document.querySelector('.media__copytext');
             copyText.select();
             document.execCommand('copy');
             copyText.blur();
-            document.querySelector('.about-us__hint').classList.add('about-us__hint_show');
+            document.querySelector('.media__hint').classList.add('media__hint_show');
             setTimeout(() => {
-                document.querySelector('.about-us__hint').classList.remove('about-us__hint_show')
+                document.querySelector('.media__hint').classList.remove('media__hint_show')
             }, 1500)
-            
         })
